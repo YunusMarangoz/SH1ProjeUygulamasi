3e420efd0e1c9e1fc1b1bb2535deb10c9e050213
Yunus <yunusmarangoz@gmail.com>
2025-09-16 22:42:13 +0300

    Add project files.

---
+++ SH1ProjeUygulamasi.WebUI/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Core.Entities;
+using SH1ProjeUygulamasi.Data;
+
+namespace SH1ProjeUygulamasi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public SlidersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SlidersController
+        public ActionResult Index()
+        {
+            return View(_context.Sliders);
+        }
+
+        // GET: SlidersController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = _context.Sliders.Find(id);
+            return View(model);
+        }
+
+        // GET: SlidersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SlidersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Slider collection, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Image is not null)
+                    {
+                        string klasor = Directory.GetCurrentDirectory() + "/wwwroot/Images/";
+                        using var stream = new FileStream(klasor + Image.FileName, FileMode.Create);
+                        Image.CopyTo(stream);
+                        collection.Image = Image.FileName;
+                    }
+                    _context.Sliders.Add(collection);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View(collection);
+        }
+
+        // GET: SlidersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = _context.Sliders.Find(id);
+            return View(model);
+        }
+
+        // POST: SlidersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Slider collection, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Image is not null)
+                    {
+                        string klasor = Directory.GetCurrentDirectory() + "/wwwroot/Images/";
+                        using var stream = new FileStream(klasor + Image.FileName, FileMode.Create);
+                        Image.CopyTo(stream);
+                        collection.Image = Image.FileName;
+                    }
+                    _context.Sliders.Update(collection);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View(collection);
+        }
+
+        // GET: SlidersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = _context.Sliders.Find(id);
+            return View(model);
+        }
+
+        // POST: SlidersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Slider collection)
+        {
+            try
+            {
+                _context.Sliders.Remove(collection);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
